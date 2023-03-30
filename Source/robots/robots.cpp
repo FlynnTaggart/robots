2ed2c7c390213a25bccbabcc353a18b4848fdf2b
Mikhail <mr1302@ya.ru>
2023-03-30 03:42:54 +0300

    Init the project

---
+++ Source/robots/robots.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "robots.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, robots, "robots" );
---
+++ Source/robots/robotsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "robotsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTS_API ArobotsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
